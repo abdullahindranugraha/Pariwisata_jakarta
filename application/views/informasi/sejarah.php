fb28c0f9533a3543959885d4cffc270a6db9bcb6
mhoer <mhoerudin82@gmail.com>
2021-12-04 19:08:14 +0700

    Menambahkan Halaman Sejarah

---
+++ application/views/informasi/sejarah.php
@@ -0,0 +1,110 @@
+<div class="banner-sejarah">
+    <h2 class="text-center">"Masa lalu adalah kenangan terbaik yang membawamu dalam kemenangan."</h2>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-2 offset-1">
+            <img src="<?php echo base_url() ?>assets/img/sejarah/ancoljadul.jpg" class="img-thumbnail" alt="">
+        </div>
+        <div class="col-sm-2">
+            <img src="<?php echo base_url() ?>assets/img/sejarah/bundaran.jpg" class="img-thumbnail" alt="">
+        </div>
+        <div class="col-sm-2">
+            <img src="<?php echo base_url() ?>assets/img/sejarah/stasiun.jpg" class="img-thumbnail" alt="">
+        </div>
+        <div class="col-sm-2">
+            <img src="<?php echo base_url() ?>assets/img/sejarah/pancoran.jpg" class="img-thumbnail" alt="">
+        </div>
+        <div class="col-sm-2">
+            <img src="<?php echo base_url() ?>assets/img/sejarah/mesjid.jpg" class="img-thumbnail" alt="">
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="content">
+        <h1><?php echo $judul ?></h1>
+        <p>Jakarta indonesia adalah ibu kota dan kota terbesar Indonesia. Terletak di estuari Sungai Ciliwung, di bagian barat laut Jawa, daerah ini telah lama menopang pemukiman manusia. Bukti bersejarah dari Jakarta berasal dari abad ke-4 M, saat ia merupakan sebuah permukiman dan pelabuhan Hindu. Kota ini telah diklaim secara berurutan oleh kerajaan bercorak India Tarumanegara, Kerajaan Sunda Hindu, Kesultanan Banten Muslim, dan oleh pemerintahan Belanda, Jepang, dan Indonesia. Hindia Belanda membangun daerah tersebut sebelum direbut oleh Kekaisaran Jepang semasa Perang Dunia II dan akhirnya menjadi merdeka dan menjadi bagian Indonesia.</p>
+        <p>Jakarta telah dikenal dengan beberapa nama. Ia disebut Sunda Kelapa selama periode Kerajaan Sunda dan Jayakarta, Djajakarta, atau Jacatra selama periode singkat Kesultanan Banten. Setelah itu, Jakarta berkembang dalam tiga tahap. "Kota Tua Jakarta", yang dekat dengan laut di utara, berkembang antara 1619 dan 1799 pada era VOC. "Kota baru" di selatan berkembang antara 1809 dan 1942 setelah pemerintah Belanda mengambil alih penguasaan Batavia dari VOC yang gagal yang sewanya telah berakhir pada 1799. Yang ketiga adalah perkembangan Jakarta modern sejak proklamasi kemerdekaan pada 1945. Di bawah pemerintahan Belanda, ia dikenal sebagai Batavia (1619–1949), dan Djakarta (dalam bahasa Belanda) atau Jakarta, selama pendudukan Jepang dan masa modern.</p>
+    </div>
+</div>
+
+<figure class="text-center">
+    <blockquote class="blockquote">
+        <p><strong>JAS MERAH</strong>. Jangan sekali - kali meninggalkan sejarah</p>
+    </blockquote>
+    <figcaption class="blockquote-footer">
+        Bung Karno
+    </figcaption>
+</figure>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/sejarah/meseum.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Museum Fatahillah</h5>
+                    <p class="card-text">Museum Fatahillah memiliki nama resmi Museum Sejarah Jakarta adalah sebuah museum yang terletak di Jalan Taman Fatahillah Nomor 1, Jakarta Barat, dengan luas lebih dari 1.300 meter persegi.</p>
+                    <a href="https://goo.gl/maps/ttE6GpSaNRY6bcB1A" class="btn btn-success stretched-link">Lokasi</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/sejarah/bank.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Museum Bank Indonesia</h5>
+                    <p class="card-text">Museum Bank Indonesia adalah sebuah museum di Jakarta, Indonesia yang terletak di Jl. Pintu Besar Utara No.3, Jakarta Barat. Peninggalan De Javasche Bank yang beraliran neo-klasikal.</p>
+                    <a href="https://g.page/museum-bi?share" class="btn btn-success stretched-link">Lokasi</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/sejarah/pki.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Museum Pengkhianatan PKI (Komunis)</h5>
+                    <p class="card-text">Museum Pengkhianatan PKI yang masih termasuk dalam kompleks Museum Lubang Buaya adalah sebuah tempat yang menceritakan sejarah pemberontakan Partai Komunis Indonesia (PKI).</p>
+                    <a href="https://goo.gl/maps/mF6DP6jjnJnN2nWn9" class="btn btn-success stretched-link">Lokasi</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/sejarah/w.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Museum Nasional Indonesia</h5>
+                    <p class="card-text">Museum Nasional Republik Indonesia atau Museum Gajah, adalah sebuah museum arkeologi, sejarah, etnografi, dan geografi yang terletak di Jakarta Pusat dan persisnya di Jalan Merdeka Barat 12. Museum ini merupakan museum pertama dan terbesar di Asia Tenggara.</p>
+                    <a href="https://g.page/Museumnasionalindonesia?share" class="btn btn-success stretched-link">Lokasi</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/sejarah/d.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Museum Bahari Jakarta</h5>
+                    <p class="card-text">Museum Bahari adalah museum yang menyimpan koleksi yang berhubungan dengan kebaharian dan kenelayanan bangsa Indonesia dari Sabang hingga Merauke yang berlokasi di seberang Pelabuhan Sunda Kelapa.</p>
+                    <a href="https://goo.gl/maps/YCrR6cvmzaGw3MNS6" class="btn btn-success stretched-link">Lokasi</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/sejarah/p.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Museum Kebangkitan Nasional</h5>
+                    <p class="card-text">Museum Kebangkitan Nasional adalah sebuah gedung yang dibangun sebagai monumen tempat lahir dan berkembangnya kesadaran nasional dan juga ditemukannya organisasi pergerakan modern pertama kali dengan nama Boedi Oetomo.</p>
+                    <a href="https://goo.gl/maps/vdMmWy2dUHJu9Ch87" class="btn btn-success stretched-link">Lokasi</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

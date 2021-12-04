891da46908f95d39d7bb132d553cee9ffec53da1
sendisupandi <sendisupandi@gmail.com>
2021-12-04 19:06:54 +0700

    menambahkan menu kuliner

---
+++ application/views/informasi/kuliner.php
@@ -0,0 +1,113 @@
+<div class="container-fluid">
+    <h1 class="text-center"><?= $judul ?></h1>
+    <center>
+        <hr>
+    </center>
+    <h5 class="d-inline p-2">Makan enak di Jakarta</h5>
+
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/kuliner/kerak.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Kerak Telor</h5>
+                    <p class="card-text">Kerak telur adalah makanan asli daerah Jakarta, dengan bahan-bahan beras ketan putih, telur ayam atau bebek, ebi yang disangrai kering ditambah bawang merah goreng, lalu diberi bumbu yang dihaluskan berupa kelapa sangrai, cabai merah, kencur, jahe, merica butiran, garam dan gula pasir.</p>
+                    <button class="btn-danger">
+                        Rp. 15.000 - 30.000
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/kuliner/soto.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Soto Betawi</h5>
+                    <p class="card-text">Soto Betawi merupakan soto yang khas dari daerah DKI Jakarta. Seperti halnya Soto Madura dan Soto Sulung, soto Betawi juga menggunakan jeroan. Selain jeroan, sering kali organ-organ lain juga disertakan, seperti mata, terpedo, dan juga hati. Daging sapi juga menjadi bahan campuran dalam soto Betawi</p>
+                    <button class="btn-danger">
+                        Rp. 50.000 - 70.000
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/kuliner/es.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Es Selendang Mayang</h5>
+                    <p class="card-text">Es selendang mayang adalah salah satu minuman tradisional Indonesia asal Jakarta. Minuman ini sekarang jarang ditemukan karena dikalangan masyarakat Betawi sendiri minuman ini dianggap minuman kuno.</p>
+                    <button class="btn-danger">
+                        Rp. 5.000 - 30.000
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/kuliner/buaya.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Roti Buaya</h5>
+                    <p class="card-text">Roti buaya adalah hidangan Betawi berupa roti manis berbentuk buaya. Roti buaya senantiasa hadir dalam upacara pernikahan dan kenduri tradisional Betawi.</p>
+                    <button class="btn-danger">
+                        Rp. 50.000 - 200.000
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/kuliner/kue.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Kue Cucur</h5>
+                    <p class="card-text">Kue Cucur atau Cucur merupakan jajanan khas suku Betawi yang berasal dari wilayah barat pulau Jawa. Kue ini terbuat dari tepung beras dan gula jawa atau gula aren yang digoreng. Kue Cucur memiliki bentuk yang cenderung tebal menggembung seperti gunung di bagian tengah dan tipis di bagian pinggirannya.</p>
+                    <button class="btn-danger">
+                        Rp. 5.000 - 15.000
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/kuliner/nasi.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Nasi Ulam</h5>
+                    <p class="card-text">Nasi ulam adalah hidangan nasi yang dicampur berbagai bumbu dan rempah, khususnya daun pegagan atau kadang diganti daun kemangi, sayuran, dan berbagai bumbu, serta ditemani beberapa macam lauk-pauk.</p>
+                    <button class="btn-danger">
+                        Rp. 10.000 - 20.000
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/kuliner/sayur.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Sayur Babanci</h5>
+                    <p class="card-text">Sayur Babanci atau Ketupat Babanci adalah salah satu hidangan ikonik khas Betawi yang kini mulai langka. Kelangkaan ini disebabkan karena bahan dan rempah-rempah untuk membuat hidangan ini sudah sulit ditemukan di Jakarta.</p>
+                    <button class="btn-danger">
+                        Rp. 10.000 - 20.000
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url() ?>assets/img/kuliner/asinan.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Asinan Betawi</h5>
+                    <p class="card-text">Asinan adalah sejenis makanan yang dibuat dengan cara pengacaran, bahan yang diacarkan yaitu berbagai jenis sayuran dan buah-buahan. Di Indonesia, asinan menjadi salah satu hidangan khas seni kuliner Indonesia.</p>
+                    <button class="btn-danger">
+                        Rp. 10.000 - 20.000
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
